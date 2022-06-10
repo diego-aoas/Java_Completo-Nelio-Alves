cf82960604652e907902bb922119922c9a407b5b
Diego <diego.aoas@gmail.com>
2022-06-10 17:27:36 -0300

    Atualização

--- src/Video35/EstruturaCondicional.java
+++ src/Video35/EstruturaCondicional.java
@@ -3,8 +3,16 @@ package Video35;
 public class EstruturaCondicional {
 
 	public static void main(String[] args) {
-		
 
+		// EXEMPLO 1
+		/*
+		 * int x = 5;
+		 * 
+		 * System.out.println("Bom dia");
+		 * 
+		 * if (x < 0) { System.out.println("Boa tarde"); }
+		 * 
+		 * System.out.println("Boa noite");
+		 */
 	}
-
 }
