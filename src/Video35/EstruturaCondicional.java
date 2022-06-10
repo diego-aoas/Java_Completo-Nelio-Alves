6215103e1b0ad51f60df78b41824f73dbd2241cd
Diego <55292470+diego-aoas@users.noreply.github.com>
2022-06-10 19:56:08 -0300

    Atualização

--- src/Video35/EstruturaCondicional.java
+++ src/Video35/EstruturaCondicional.java
@@ -1,8 +1,12 @@
 package Video35;
 
+import java.util.Scanner;
+
 public class EstruturaCondicional {
 
 	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
 
 		// EXEMPLO 1
 		/*
@@ -14,7 +18,22 @@ public class EstruturaCondicional {
 		 * 
 		 * System.out.println("Boa noite");
 		 */
-		
-		int i=0;
+
+		// EXEMPLO 2
+
+		int hora;
+
+		System.out.print("Quantas horas? ");
+		hora = sc.nextInt();
+
+		if (hora < 12) {
+			System.out.println("Bom dia");
+		} else if (hora < 18) {
+			System.out.println("Boa tarde");
+		} else {
+			System.out.println("Boa noite");
+		}
+
+		sc.close();
 	}
 }
