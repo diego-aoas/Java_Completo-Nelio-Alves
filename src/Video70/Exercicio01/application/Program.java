018367e0482330918895a1e2618b4dd6e0f55fc2
Diego <55292470+diego-aoas@users.noreply.github.com>
2022-07-28 17:48:41 -0300

    Atualização

---
+++ src/Video70/Exercicio01/application/Program.java
@@ -0,0 +1,30 @@
+package Video70.Exercicio01.application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import Video70.Exercicio01.entities.Rectangle;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		Rectangle rectangle = new Rectangle();
+
+		System.out.println("Enter rectangle width and height:");
+		rectangle.widtht = sc.nextDouble();
+		rectangle.height = sc.nextDouble();
+
+		System.out.printf("AREA      = %.2f%n", rectangle.area());
+
+		System.out.printf("PERIMETER = %.2f%n", rectangle.perimeter());
+
+		System.out.printf("DIAGONAL  = %.2f%n", rectangle.diagonal());
+
+		sc.close();
+	}
+
+}
---
+++ src/Video70/Exercicio03/application/Program.java
@@ -0,0 +1,9 @@
+package Video70.Exercicio03.application;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
