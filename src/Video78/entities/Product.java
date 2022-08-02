83d399dfa66e9d8c9c14a756aee3229b16ec7072
Diego <diego.aoas@gmail.com>
2022-08-02 13:45:59 -0300

    Atualização

---
+++ src/Video78/entities/Product.java
@@ -0,0 +1,43 @@
+package Video78.entities;
+
+public class Product {
+
+	public String name;
+	public double price;
+	public int quantity;
+
+	public Product() {
+
+	}
+
+	public Product(String name, double price, int quantity) { // Construtor
+		this.name = name;
+		this.price = price;
+		this.quantity = quantity;
+	}
+
+	public Product(String name, double price) { // Construtor
+		this.name = name;
+		this.price = price;
+		// quantity = 0; ===> É opcional declarar ou não. E a variável não precisa
+		// do"this".
+
+	}
+
+	public double totalValueInStock() {
+		return price * quantity;
+	}
+
+	public void addProducts(int quantity) {
+		this.quantity += quantity; // this = referência para o atributo da classe;
+	}
+
+	public void removeProducts(int quantity) {
+		this.quantity -= quantity; // novamente this referenciando o atributo da classe;
+	}
+
+	public String toString() {
+		return name + ", $ " + String.format("%.2f", price) + ", " + quantity + " units, Total: $ "
+				+ String.format("%.2f", (quantity * price));
+	}
+}
