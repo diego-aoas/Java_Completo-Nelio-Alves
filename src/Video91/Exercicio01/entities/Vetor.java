296983eedb75e482aae9c75970a8f31b034c8882
Diego <diego.aoas@gmail.com>
2022-08-08 18:58:17 -0300

    Atualização

--- src/Video91/Exercicio01/entities/Vetor.java
+++ src/Video91/Exercicio01/entities/Vetor.java
@@ -1,31 +1,18 @@
 package Video91.Exercicio01.entities;
 
 public class Vetor {
-    private int valor;
-    private int nElementos;
+	private int valor;
 
-    public Vetor(int valor) {
-        this.valor = valor;
-    }
+	public Vetor(int valor) {
+		this.valor = valor;
+	}
 
-    public int getValor() {
-        return valor;
-    }
+	public int getValor() {
+		return valor;
+	}
 
-    public void setValor(int valor) {
-        this.valor = valor;
-    }
-
-    public void negativos(int nElementos) {
-        int[] vet = new int[nElementos];
-
-        for (int i = 0; i < vet.length; i++) {
-            if (vet[i].getValor() < 0) {
-                vet[i].getValor();
-            }
-        }
-        return 0;
-    }
+	public void setValor(int valor) {
+		this.valor = valor;
+	}
 
 }
-
