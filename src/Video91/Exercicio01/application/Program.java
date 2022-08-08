08119adf125ce7d262fa74b8ca107dd2c2ad5eac
Diego <55292470+diego-aoas@users.noreply.github.com>
2022-08-08 18:58:17 -0300

    Atualização

--- src/Video91/Exercicio01/application/Program.java
+++ src/Video91/Exercicio01/application/Program.java
@@ -4,37 +4,38 @@ import Video91.Exercicio01.entities.Vetor;
 
 import java.util.Scanner;
 
-
 public class Program {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 
-        int nElementos = 0;
+		int nElementos = 0;
 
-        System.out.print("Quantos numeros você vai digitar ?: ");
-        nElementos = sc.nextInt();
+		System.out.print("Quantos numeros você vai digitar ?: ");
+		nElementos = sc.nextInt();
 
-        while (nElementos > 10) {
-            System.out.println("O máximo de elementos permitido são 10...");
-            System.out.print("Quantos numeros você vai digitar ?: ");
-            nElementos = sc.nextInt();
-        }
+		while (nElementos > 10) {
+			System.out.println("O máximo de elementos permitido são 10...");
+			System.out.print("Quantos numeros você vai digitar ?: ");
+			nElementos = sc.nextInt();
+		}
 
-        Vetor[] vet = new Vetor[nElementos];
+		Vetor[] vet = new Vetor[nElementos];
 
-        for (int i = 0; i < vet.length; i++) {
-            System.out.print("Digite um número: ");
-            int valor = sc.nextInt();
+		for (int i = 0; i < vet.length; i++) {
+			System.out.print("Digite um número: ");
+			int valor = sc.nextInt();
+			vet[i] = new Vetor(valor);
 
-            vet[i] = new Vetor(valor);
+		}
 
-        }
-        System.out.print(".");
-        vet[].getValor();
+		System.out.println("NUMEROS NEGATIVOS: ");
+		for (int i = 0; i < vet.length; i++) {
+			if (vet[i].getValor() < 0) {
+				System.out.println(vet[i].getValor());
+			}
+		}
 
-        System.out.printf("NUMEROS NEGATIVOS: " + vet.negativos());
-
-        sc.close();
-    }
+		sc.close();
+	}
 }
