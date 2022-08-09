574d6f70092c5d53172b6daa01e549e0490895a0
Diego <diego.aoas@gmail.com>
2022-08-08 22:05:31 -0300

    Atualização

---
+++ src/Video91/Exercicio02/entities/VetorOperacoes.java
@@ -0,0 +1,18 @@
+package Video91.Exercicio02.entities;
+
+public class VetorOperacoes {
+    private double valor;
+
+    public VetorOperacoes(double valor) {
+        this.valor = valor;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+}
