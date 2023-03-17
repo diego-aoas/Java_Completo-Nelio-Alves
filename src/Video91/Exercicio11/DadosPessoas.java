c283d50b9777ec0862ee935904f308a9589a8404
Diego <55292470+diego-aoas@users.noreply.github.com>
2023-03-17 02:06:15 -0300

    Atualização

--- src/Video91/Exercicio11/DadosPessoas.java
+++ src/Video91/Exercicio11/DadosPessoas.java
@@ -12,24 +12,41 @@ public class DadosPessoas {
         int n = sc.nextInt();
 
         int masc = 0, fem = 0;
+        double menorAltura = Byte.MAX_VALUE, maiorAltura = Byte.MIN_VALUE, mediaAltF = 0;
         double[] altura = new double[n];
-        String[] genero = new String[n];
+        char[] genero = new char[n];
 
         for (int i = 0; i < n; i++) {
-            System.out.printf("Altura da %do pessoa: ", i);
+            System.out.printf("Altura da %da pessoa: ", i + 1);
             altura[i] = sc.nextDouble();
 
-            System.out.printf("Genero da %do pessoa: ", i);
-            genero[i] = sc.nextLine();
+            System.out.printf("Genero da %da pessoa: ", i + 1);
+            genero[i] = sc.next().toUpperCase().charAt(0);
 
-            if (genero[i] == "F") {
+            if (genero[i] == 'M') {
+                masc += 1;
+            } else {
+                mediaAltF += altura[i];
                 fem += 1;
-            else{
-                    masc += 1;
-                }
             }
 
+
+            if (altura[i] > maiorAltura) {
+                maiorAltura = altura[i];
+            }
+
+            if (altura[i] < menorAltura) {
+                menorAltura = altura[i];
+            }
         }
+
+        mediaAltF = mediaAltF / fem;
+
+        System.out.println("Menor altura foi: " + menorAltura);
+        System.out.println("Maior altura foi: " + maiorAltura);
+        System.out.printf("Média das alturas das mulheres: %.2f\n", mediaAltF);
+        System.out.println("Numero de homens: " + masc);
+
 
         sc.close();
     }
