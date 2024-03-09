c893514120ee051a46b33e0fa5a7002216e909a1
Diego <55292470+diego-aoas@users.noreply.github.com>
2024-03-09 01:07:10 -0300

    Atualização

--- src/Video156_Heranca/application/Program.java
+++ src/Video156_Heranca/application/Program.java
@@ -1,7 +1,6 @@
 package Video156_Heranca.application;
 
 import Video156_Heranca.entities.Account;
-import Video156_Heranca.entities.BusinessAccount;
 import Video156_Heranca.entities.SavingsAccount;
 
 public class Program {
@@ -35,6 +34,7 @@ public class Program {
         }
 */
 
+        /*
         Account acc1 = new Account(1001, "Alex", 1000.0);
         acc1.withdraw(200.0);
         System.out.println(acc1.getBalance());
@@ -46,6 +46,16 @@ public class Program {
         Account acc3 = new BusinessAccount(1003, "Bob", 1000.0, 500.0);
         acc3.withdraw(200.0);
         System.out.println(acc3.getBalance());
+*/
+
+        Account x = new Account(1020, "Alex", 1000.0);
+        Account y = new SavingsAccount(1023, "Maria", 1000.0, 0.01);
+
+        x.withdraw(50.0);
+        y.withdraw(50.0);
+
+        System.out.println(x.getBalance());
+        System.out.println(y.getBalance());
 
     }
 }
