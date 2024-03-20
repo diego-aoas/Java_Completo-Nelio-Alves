2b23f62752c3e1870040788428c5e81c5e6f762c
Diego <diego.aoas@gmail.com>
2024-03-20 00:07:36 -0300

    Seção 15: Tratamento de Exceções

---
+++ src/Video174_ExercicioFixacao/application/Program.java
@@ -0,0 +1,33 @@
+package Video174_ExercicioFixacao.application;
+
+import Video174_ExercicioFixacao.model.entities.Account;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter account data");
+        System.out.print("Number: ");
+        int num = sc.nextInt();
+        System.out.print("Holder: ");
+        String hold = sc.nextLine();
+        System.out.print("Initial balance: ");
+        Double balance = sc.nextDouble();
+        System.out.print("Withdraw limit: ");
+        Double wLimit = sc.nextDouble();
+        System.out.print("Enter amount for withdraw: 100.00: ");
+        Double amount = sc.nextDouble();
+
+        Account acc = new Account(num, hold, balance, wLimit);
+
+/       System.out.println("Enter amount for withdraw: " + acc.getBalance());
+        //terminar
+
+        sc.close();
+    }
+}
